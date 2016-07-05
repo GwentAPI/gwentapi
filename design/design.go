eef1fcf5b1fd84d2e869df3ae35f6d92adff2068
Tristan <kraig.tri125@gmail.com>
2016-07-05 19:22:12 -0400

    Initial

---
+++ design/design.go
@@ -0,0 +1,39 @@
+package design
+
+import (
+	. "github.com/goadesign/goa/design"
+	. "github.com/goadesign/goa/design/apidsl"
+)
+
+var _ = API("gwentapi", func() {
+	Title("Gwent API")
+	Description("A consumption-only API of all Gwent data")
+	BasePath("/")
+	Host("localhost:8080")
+	Scheme("https")
+})
+
+var _ = Resource("faction", func() {
+	DefaultMedia(FactionMedia)
+	BasePath("/factions")
+
+	Action("show", func() {
+		Description("Return faction with given id")
+		Routing(GET("/:factionID"))
+		Params(func() {
+			Param("factionID", String, "Faction ID")
+		})
+		Response(OK)
+		Response(NotFound)
+
+	})
+})
+
+var FactionMedia = MediaType("application/vnd.gwentapi.faction+json", func() {
+	Description("A faction")
+	Attributes(func() {
+		Attribute("id", Integer, "Unique faction ID")
+		Attribute("href")
+		Attribute("name")
+	})
+})
