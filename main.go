9811ca2dc1a7e4e7d6e944e3c88ddcf356395c79
Tristan <kraig.tri125@gmail.com>
2016-07-27 13:44:50 -0400

    Adding variables to switch between debug/prod and others. Adding TLS call

--- main.go
+++ main.go
@@ -10,16 +10,26 @@ import (
 	"github.com/tri125/gwentapi/controllers"
 )
 
+var isDebug bool = true
+var isVerbose bool = false
+var enableGzip bool = true
+var gzipLevel int = -1
+
+var certFile string = "pathToCertFile"
+var keyFile string = "pathToKeyFile"
+
 func main() {
 	// Create service
 	service := goa.New("gwentapi")
 
 	// Mount middleware
 	service.Use(middleware.RequestID())
-	service.Use(middleware.LogRequest(true))
-	service.Use(middleware.ErrorHandler(service, true))
+	service.Use(middleware.LogRequest(isVerbose))
+	service.Use(middleware.ErrorHandler(service, isVerbose))
 	service.Use(middleware.Recover())
-	service.Use(gzip.Middleware(-1))
+	if enableGzip {
+		service.Use(gzip.Middleware(gzipLevel))
+	}
 	// Mount "card" controller
 	c := NewCardController(service)
 	app.MountCardController(service, c)
@@ -51,7 +61,15 @@ func main() {
 	defer controllers.DBCon.Close()
 
 	// Start service
-	if err := service.ListenAndServe(":8080"); err != nil {
-		service.LogError("startup", "err", err)
+
+	if isDebug {
+		if err := service.ListenAndServe(":8080"); err != nil {
+			service.LogError("startup", "err", err)
+		}
+	} else {
+		if err := service.ListenAndServeTLS(":8080", "certFile", "keyfile"); err != nil {
+			service.LogError("startup", "err", err)
+		}
 	}
+
 }
