91002ca16d3f526a83a313574e47d895df3c3dc3
Tristan <kraig.tri125@gmail.com>
2016-08-13 22:21:23 -0400

    Wrapper and workaround to set the timeout to the database connection from the toml config file.

--- configuration/config.go
+++ configuration/config.go
@@ -4,6 +4,7 @@ import (
 	"github.com/BurntSushi/toml"
 	"github.com/go-sql-driver/mysql"
 	"log"
+	"time"
 )
 
 var Conf GwentConfig
@@ -16,7 +17,20 @@ type GwentConfig struct {
 }
 
 type server struct {
-	BaseURL string
+	BaseURL    string
+	DB_timeout timeout
+}
+
+//Wrapper around time.Duration
+type timeout struct {
+	time.Duration
+}
+
+//Implement UnmarshalText so that the TOML parser can work its magic on timeout
+func (t *timeout) UnmarshalText(text []byte) error {
+	var err error
+	t.Duration, err = time.ParseDuration(string(text))
+	return err
 }
 
 // Reads info from config file
@@ -29,11 +43,13 @@ func ReadConfig() error {
 		log.Println(err)
 		return err
 	}
+	//Workaround to set the database connection timeout in the mysql.Config type because the TOML parser doesn't implicitly parse Duration.
+	//We use a wrapper and then set the value back.
+	Conf.DB.Timeout = Conf.Server.DB_timeout.Duration
 	isLoaded = true
 	return nil
 }
 
 func (g GwentConfig) FormatDSN() string {
-	//log.Println(g.DB.FormatDSN())
 	return g.DB.FormatDSN()
 }
