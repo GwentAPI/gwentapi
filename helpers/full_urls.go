4d936365a269b0ee7ec7376a9b0999abcbecab56
Tristan <kraig.tri125@gmail.com>
2017-06-18 02:29:08 -0400

    Improved config loading logic. Change in config file format. Sane loading and generation of default config file if missing.

--- helpers/full_urls.go
+++ helpers/full_urls.go
@@ -6,27 +6,29 @@ import (
 	"github.com/GwentAPI/gwentapi/configuration"
 )
 
+var baseURL string
+
 // CardHref returns the resource href.
 func CardURL(cardID interface{}) string {
-	return configuration.Conf.Server.BaseURL + app.CardHref(cardID)
+	return baseURL + app.CardHref(cardID)
 }
 
 // FactionHref returns the resource href.
 func FactionURL(factionID interface{}) string {
-	return configuration.Conf.Server.BaseURL + app.FactionHref(factionID)
+	return baseURL + app.FactionHref(factionID)
 }
 
 func VariationURL(cardID interface{}, variationID interface{}) string {
-	return fmt.Sprintf("%v/v0/cards/%v/variations/%v", configuration.Conf.Server.BaseURL, cardID, variationID)
+	return fmt.Sprintf("%v/v0/cards/%v/variations/%v", baseURL, cardID, variationID)
 }
 
 func CategoryURL(categoryUUID interface{}) string {
-	return configuration.Conf.Server.BaseURL + app.CategoryHref(categoryUUID)
+	return baseURL + app.CategoryHref(categoryUUID)
 }
 
 // GroupHref returns the resource href.
 func GroupURL(groupID interface{}) string {
-	return configuration.Conf.Server.BaseURL + app.GroupHref(groupID)
+	return baseURL + app.GroupHref(groupID)
 }
 func SwaggerURL() string {
 	return "https://gwentapi.com/swagger/"
@@ -34,10 +36,14 @@ func SwaggerURL() string {
 
 // RarityHref returns the resource href.
 func RarityURL(rarityID interface{}) string {
-	return configuration.Conf.Server.BaseURL + app.RarityHref(rarityID)
+	return baseURL + app.RarityHref(rarityID)
 }
 
 // TypeHref returns the resource href.
 func MediaURL(filename interface{}) string {
-	return fmt.Sprintf("%v/media/%v", configuration.Conf.Server.BaseURL, filename)
+	return fmt.Sprintf("%v/media/%v", baseURL, filename)
+}
+
+func init() {
+	baseURL = configuration.GetConfig().App.BaseURL
 }
