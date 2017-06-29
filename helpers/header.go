ef0cb62ff4233faf378d098a3e5ddc847b1f7b9a
Tristan <kraig.tri125@gmail.com>
2017-06-29 06:33:12 -0400

    Function to compare time.

--- helpers/header.go
+++ helpers/header.go
@@ -13,3 +13,16 @@ func LastModified(data *goa.ResponseData, date time.Time) {
 func setHeader(data *goa.ResponseData, header string, value time.Time) {
 	data.Header().Set(header, value.UTC().Format(http.TimeFormat))
 }
+
+func isModified(ifNotModifiedSince string, lastModified time.Time) bool {
+	lastModified = lastModified.UTC()
+	timeIfNotModifiedSince, err := http.ParseTime(ifNotModifiedSince)
+
+	if err == nil && timeIfNotModifiedSince.Before(lastModified) {
+		return true
+	} else if err != nil {
+		return true
+	} else {
+		return false
+	}
+}
